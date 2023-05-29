1d0813f16cc10eed63a83935ad33f207289dbee1
ThinkQAQ <aetejay@gmail.com>
2023-05-29 21:48:15 +0800

    Add ignore recording channel message edit/delete command

--- cogs/msg_manage.py
+++ cogs/msg_manage.py
@@ -1,7 +1,7 @@
 import disnake
 from disnake.ext import commands, tasks
 from core.any import CogExtension
-from disnake import ui, Event
+from disnake import ui, Event, Embed
 import json
 from datetime import datetime, timedelta, timezone
 from util.audit_var import AuditData
@@ -37,7 +37,7 @@ class MsgManage(CogExtension):
     def __init__(self, bot):
         super().__init__(bot)
         self.clean_audit_variable.start()
-        self.bot_delete_edit = False     # whether is bot delete/edit message
+        self.bot_delete_edit = False  # whether is bot delete/edit message
 
     @staticmethod
     def store_channel_data():
@@ -63,16 +63,27 @@ class MsgManage(CogExtension):
 
     @commands.slash_command(description="設定訊息刪除紀錄頻道", dm_permission=False)
     @commands.default_member_permissions(administrator=True)
-    async def set_msg_delete_ch(self, inter, channel_id: int = commands.Param(name="頻道id", large=True)):
+    async def set_msg_delete_ch(self, inter,
+                                channel_id: int = commands.Param(name="頻道id", description="輸入0以清除頻道設定",
+                                                                 large=True)):
         guild = inter.guild
         answer = YesNoButton()
 
+        if channel_id == 0:
+            data[str(guild.id)]["msg_delete_channel_id"] = ""
+            self.store_channel_data()
+            await inter.response.send_message("已清除設定")
+            return
+        if guild.get_channel(channel_id) is None:
+            await inter.response.send_message("找不到頻道")
+            return
         if str(guild.id) in data:
             def check(i: disnake.MessageInteraction):
                 return i.message.id == msg.id and i.author == inter.author
 
             if data[str(guild.id)]["msg_delete_channel_id"] != "":
-                await inter.response.send_message("此伺服器已經設定過刪除訊息通知頻道\n是否要重新設置(y/n)", view=answer)
+                await inter.response.send_message("此伺服器已經設定過刪除訊息通知頻道\n是否要重新設置(y/n)",
+                                                  view=answer)
                 msg = await inter.original_response()
                 await self.bot.wait_for(Event.button_click, check=check)
                 if not answer.value:
@@ -80,7 +91,7 @@ class MsgManage(CogExtension):
             data[str(guild.id)]["msg_delete_channel_id"] = str(channel_id)
             self.store_channel_data()
         else:
-            data[str(guild.id)] = {"msg_edit_channel_id": "", "msg_delete_channel_id": ""}
+            data[str(guild.id)] = {"msg_edit_channel_id": "", "msg_delete_channel_id": "", "non-listening_channel": []}
             data[str(guild.id)]["msg_delete_channel_id"] = str(channel_id)
             self.store_channel_data()
         if inter.response.is_done():
@@ -90,16 +101,27 @@ class MsgManage(CogExtension):
 
     @commands.slash_command(description="設定訊息編輯紀錄頻道", dm_permission=False)
     @commands.default_member_permissions(administrator=True)
-    async def set_msg_edit_ch(self, inter, channel_id: int = commands.Param(name="頻道id", large=True)):
+    async def set_msg_edit_ch(self, inter,
+                              channel_id: int = commands.Param(name="頻道id", description="輸入0以清除頻道設定",
+                                                               large=True)):
         guild = inter.guild
         answer = YesNoButton()
 
+        if channel_id == 0:
+            data[str(guild.id)]["msg_edit_channel_id"] = ""
+            self.store_channel_data()
+            await inter.response.send_message("已清除設定")
+            return
+        if guild.get_channel(channel_id) is None:
+            await inter.response.send_message("找不到頻道")
+            return
         if str(guild.id) in data:
             def check(i: disnake.MessageInteraction):
                 return i.message.id == msg.id and i.author == inter.author
 
             if data[str(guild.id)]["msg_edit_channel_id"] != "":
-                await inter.response.send_message("此伺服器已經設定過編輯訊息通知頻道\n是否要重新設置(y/n)", view=answer)
+                await inter.response.send_message("此伺服器已經設定過編輯訊息通知頻道\n是否要重新設置(y/n)",
+                                                  view=answer)
                 msg = await inter.original_response()
                 await self.bot.wait_for(Event.button_click, check=check)
                 if not answer.value:
@@ -107,7 +129,7 @@ class MsgManage(CogExtension):
             data[str(guild.id)]["msg_edit_channel_id"] = str(channel_id)
             self.store_channel_data()
         else:
-            data[str(guild.id)] = {"msg_edit_channel_id": "", "msg_delete_channel_id": ""}
+            data[str(guild.id)] = {"msg_edit_channel_id": "", "msg_delete_channel_id": "", "non-listening_channel": []}
             data[str(guild.id)]["msg_edit_channel_id"] = str(channel_id)
             self.store_channel_data()
         if inter.response.is_done():
@@ -115,12 +137,80 @@ class MsgManage(CogExtension):
         else:
             await inter.response.send_message("設置成功")
 
+    @commands.slash_command(description="設定*不監聽訊息編輯/刪除*頻道", dm_permission=False)
+    @commands.default_member_permissions(administrator=True)
+    async def set_non_listening_msg_ch(self, inter, channel_id: int = commands.Param(name="頻道id", large=True)):
+        guild = inter.guild
+
+        if guild.get_channel(channel_id) is None:
+            await inter.response.send_message("找不到頻道")
+            return
+        if str(guild.id) in data:
+            if str(channel_id) not in data[str(guild.id)]["non-listening_channel"]:
+                data[str(guild.id)]["non-listening_channel"].append(str(channel_id))
+                self.store_channel_data()
+        else:
+            data[str(guild.id)] = {"msg_edit_channel_id": "", "msg_delete_channel_id": "", "non-listening_channel": []}
+            data[str(guild.id)]["non-listening_channel"].append(str(channel_id))
+            self.store_channel_data()
+        await inter.response.send_message("設置成功")
+
+    @commands.slash_command(description="移除*不監聽訊息編輯/刪除*頻道", dm_permission=False)
+    @commands.default_member_permissions(administrator=True)
+    async def remove_non_listening_msg_ch(self, inter, channel_id: int = commands.Param(name="頻道id", large=True)):
+        guild = inter.guild
+
+        if str(guild.id) in data:
+            if str(channel_id) in data[str(guild.id)]["non-listening_channel"]:
+                data[str(guild.id)]["non-listening_channel"].remove(str(channel_id))
+            else:
+                await inter.response.send_message("頻道未被設定成不監聽")
+                return
+            self.store_channel_data()
+            await inter.response.send_message("移除成功")
+        else:
+            data[str(guild.id)] = {"msg_edit_channel_id": "", "msg_delete_channel_id": "", "non-listening_channel": []}
+            self.store_channel_data()
+            await inter.response.send_message("頻道未被設定成不監聽")
+
+    @commands.slash_command(description="列出訊息管理設定頻道", dm_permission=False)
+    @commands.default_member_permissions(administrator=True)
+    async def list_msg_manage_setting(self, inter):
+        guild = inter.guild
+
+        if str(guild.id) not in data:
+            await inter.response.send_message("伺服器沒有設定訊息管理")
+            return
+        edit_ch = "None" if data[str(guild.id)]["msg_edit_channel_id"] == "" else "<#" + data[str(guild.id)]["msg_edit_channel_id"] + ">"
+        del_ch = "None" if data[str(guild.id)]["msg_delete_channel_id"] == "" else "<#" + data[str(guild.id)]["msg_delete_channel_id"] + ">"
+        if not data[str(guild.id)]["non-listening_channel"]:
+            non_listen = "None"
+        else:
+            non_listen = ""
+            for ch in data[str(guild.id)]["non-listening_channel"]:
+                temp = "<#" + ch + ">\n"
+                non_listen += temp
+        t = timezone(timedelta(hours=8))
+        timestamp = datetime.now(t)
+
+        embed = disnake.Embed(title="Message manage setting", color=0x00bfff)
+        embed.set_author(name=inter.author, icon_url=inter.author.avatar.url)
+        embed.add_field(name="Message edit channel", value=edit_ch, inline=True)
+        embed.add_field(name="Message delete channel", value=del_ch, inline=False)
+        embed.add_field(name="Non-listening channel", value=non_listen, inline=False)
+        embed.set_footer(text="\u200b")
+        embed.timestamp = timestamp
+
+        await inter.response.send_message(embed=embed)
+
     @commands.Cog.listener("on_raw_message_delete")
     async def msg_delete(self, payload: disnake.RawMessageDeleteEvent):
         if str(payload.guild_id) not in data:
             return
         del_ch = data[str(payload.guild_id)]["msg_delete_channel_id"]
         if del_ch == "" or del_ch == str(payload.channel_id):
+            return
+        if str(payload.channel_id) in data[str(payload.guild_id)]["non-listening_channel"]:
             return
         if self.bot_delete_edit:
             return
@@ -165,6 +255,8 @@ class MsgManage(CogExtension):
             return
         edit_ch = data[str(payload.guild_id)]["msg_edit_channel_id"]
         if edit_ch == "" or edit_ch == str(payload.channel_id):
+            return
+        if str(payload.channel_id) in data[str(payload.guild_id)]["non-listening_channel"]:
             return
         if self.bot_delete_edit or payload.cached_message.author == self.bot.user:
             return
